f65b547a5e9308de7a1ea3b8df703bc7d64e4e2b
rk <ranjitkale0001@gmail.com>
2024-08-30 22:44:54 +0530

    create a database in mysql
    mysql> create database hospital_management_system;

--- Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/HospitalManagementSystemApplication.java
+++ Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/HospitalManagementSystemApplication.java
@@ -8,7 +8,7 @@ public class HospitalManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalManagementSystemApplication.class, args);
-		System.out.println("Hello");
+//		System.out.println("Hello");
 	}
 
 }
