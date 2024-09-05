53ddfe413410168367d1671c194c08f1efd4c5c2
rk <ranjitkale0001@gmail.com>
2024-09-06 00:43:14 +0530

    medicines value insert
    post:-
    
    http://localhost:8084/api/v3/insert
    {
    "drugName": "paricitomol",
    "stock": "21"
    
    }

--- Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/dologin/controller/MedicineController.java
+++ Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/dologin/controller/MedicineController.java
@@ -1,5 +1,28 @@
 package org.HMSApp.Hospital_Management_System.dologin.controller;
 
-public class MedicineController {
+import java.util.List;
 
+import org.HMSApp.Hospital_Management_System.dologin.entity.Appointment;
+import org.HMSApp.Hospital_Management_System.dologin.entity.Medicine;
+import org.HMSApp.Hospital_Management_System.dologin.repository.MedicineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v3")
+public class MedicineController {
+  @Autowired
+  MedicineRepository medicineRepository;
+  
+  @PostMapping("/insert")
+  public Medicine createMedicine(@RequestBody Medicine medicine) {
+	return medicineRepository.save(medicine);  
+  }
+  
+ 
+  
 }
