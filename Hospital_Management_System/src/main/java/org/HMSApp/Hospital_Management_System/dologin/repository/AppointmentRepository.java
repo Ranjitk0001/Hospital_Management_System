86e82f71655b2f352c188ac854823a10c762c582
rk <ranjitkale0001@gmail.com>
2024-09-05 22:40:57 +0530

    Adding data in database 1

---
+++ Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/dologin/repository/AppointmentRepository.java
@@ -0,0 +1,9 @@
+package org.HMSApp.Hospital_Management_System.dologin.repository;
+
+import org.HMSApp.Hospital_Management_System.dologin.entity.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+}
