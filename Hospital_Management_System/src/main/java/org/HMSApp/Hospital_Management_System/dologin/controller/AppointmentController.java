3264646072278c382454d91b752c14cc84a6a1ab
rk <ranjitkale0001@gmail.com>
2024-09-06 00:47:32 +0530

    show List Appointment
    Get:
    
    http://localhost:8084/api/v2/showlist

--- Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/dologin/controller/AppointmentController.java
+++ Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/dologin/controller/AppointmentController.java
@@ -1,8 +1,11 @@
 package org.HMSApp.Hospital_Management_System.dologin.controller;
 
+import java.util.List;
+
 import org.HMSApp.Hospital_Management_System.dologin.entity.Appointment;
 import org.HMSApp.Hospital_Management_System.dologin.repository.AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +23,10 @@ public class AppointmentController {
 		
 		return appointmentRepository.save(appointment);
 	}
+	
+	 @GetMapping("/showlist")
+	  public List<Appointment> getAllAppointments(){
+		  return appointmentRepository.findAll();
+	  }
 
 }
