c6239e5f6de9d913b8913173fce48a9dd84067c6
rk <ranjitkale0001@gmail.com>
2024-09-10 15:59:38 +0530

    spring boot to angular
    Connect to angular using this command
    
    @CrossOrigin(origins ="http://localhost:4200/")

--- Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/controller/PatientController.java
+++ Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/controller/PatientController.java
@@ -1,8 +1,13 @@
 package org.HMSApp.Hospital_Management_System.controller;
 
+import java.util.List;
+
+import org.HMSApp.Hospital_Management_System.dologin.entity.Appointment;
 import org.HMSApp.Hospital_Management_System.entity.Patient;
 import org.HMSApp.Hospital_Management_System.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+////Connect to Angular
+@CrossOrigin(origins ="http://localhost:4200/")
+
+//Connect to Angular
+//@CrossOrigin("*")
 public class PatientController {
     @Autowired
 	private PatientRepository patientRepository;
@@ -17,4 +27,10 @@ public class PatientController {
     public Patient createPatient(@RequestBody Patient patient) {
     	return patientRepository.save(patient);
     }
+    
+    @GetMapping()
+//    @GetMapping("/showlist")
+	  public List<Patient> getAllPatients(){
+		  return patientRepository.findAll();
+	  }
 }
