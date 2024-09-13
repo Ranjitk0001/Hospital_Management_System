e7fb2880f2816bf30dbccd44be23d53973791f1c
rk <ranjitkale0001@gmail.com>
2024-09-13 20:10:32 +0530

    DeleteAdmin
    Delete Patients

--- Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/controller/PatientController.java
+++ Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/controller/PatientController.java
@@ -1,13 +1,19 @@
 package org.HMSApp.Hospital_Management_System.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.HMSApp.Hospital_Management_System.dologin.entity.Appointment;
+import javax.management.AttributeNotFoundException;
 import org.HMSApp.Hospital_Management_System.entity.Patient;
 import org.HMSApp.Hospital_Management_System.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 //Connect to Angular
 //@CrossOrigin("*")
 public class PatientController {
+	
     @Autowired
 	private PatientRepository patientRepository;
     @PostMapping("/insert")
@@ -33,4 +40,15 @@ public class PatientController {
 	  public List<Patient> getAllPatients(){
 		  return patientRepository.findAll();
 	  }
+    
+    @DeleteMapping("/{id}")
+	 public ResponseEntity<Map<String,Boolean>>deleteAdmin(@PathVariable long id) throws AttributeNotFoundException{
+		 Patient patient=patientRepository.findById(id).orElseThrow(()->new AttributeNotFoundException("Admin not found with id " + id));
+		 patientRepository.delete(patient);
+		 
+		 Map<String ,Boolean> responnse= new HashMap<String,Boolean>();
+		 responnse.put("Deleted", Boolean.TRUE);
+		 
+		 return  ResponseEntity.ok(responnse);
+	 }
 }
