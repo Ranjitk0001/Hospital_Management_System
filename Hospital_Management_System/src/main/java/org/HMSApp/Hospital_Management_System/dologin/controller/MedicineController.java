cba564efb053baee28853bef3eab8186a673a6f4
rk <ranjitkale0001@gmail.com>
2024-09-14 13:17:01 +0530

    Medicine spring boot to angular

--- Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/dologin/controller/MedicineController.java
+++ Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/dologin/controller/MedicineController.java
@@ -6,6 +6,7 @@ import org.HMSApp.Hospital_Management_System.dologin.entity.Appointment;
 import org.HMSApp.Hospital_Management_System.dologin.entity.Medicine;
 import org.HMSApp.Hospital_Management_System.dologin.repository.MedicineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v3")
+//Connect to Angular
+@CrossOrigin("*")
 public class MedicineController {
   @Autowired
   MedicineRepository medicineRepository;
+  
+  @GetMapping()
+  public List<Medicine> getAllMedicine(){
+		return medicineRepository.findAll();
+  }
   
   @PostMapping("/insert")
   public Medicine createMedicine(@RequestBody Medicine medicine) {
