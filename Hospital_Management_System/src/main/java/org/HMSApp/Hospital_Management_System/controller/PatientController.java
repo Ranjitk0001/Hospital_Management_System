d1f54e92ce06818b6750d99c54044d612becdea9
rk <ranjitkale0001@gmail.com>
2024-09-14 17:52:06 +0530

    Update Patient

--- Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/controller/PatientController.java
+++ Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/controller/PatientController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,5 +52,25 @@ public class PatientController {
 		 responnse.put("Deleted", Boolean.TRUE);
 		 
 		 return  ResponseEntity.ok(responnse);
+	 }
+    
+     @PutMapping("/{id}")
+	 public ResponseEntity<Patient>updatePatient(@PathVariable long id,@RequestBody Patient patientDetails) throws AttributeNotFoundException{
+		 Patient patient=patientRepository.findById(id).orElseThrow(()->new AttributeNotFoundException("Patient not found with id " + id));
+		
+		 patient.setAge(patientDetails.getAge());
+		 patient.setName(patientDetails.getName());
+		 patient.setBlood(patientDetails.getBlood());
+		 patient.setDose(patientDetails.getDose());
+		 patient.setFees(patientDetails.getFees());
+		 patient.setPrescription(patientDetails.getPrescription());
+		 patient.setUrgency(patientDetails.getUrgency());
+//		 patient.setId(patientDetails.getId());
+		 
+		 Patient savedPatient= patientRepository.save(patient);
+		 
+		 return ResponseEntity.ok(savedPatient);
+		 
+		
 	 }
 }
