291c9c25dbc8212bdec97df14d7c092d5abf324e
rk <ranjitkale0001@gmail.com>
2024-09-15 09:23:50 +0530

    Update Medicine

--- Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/dologin/controller/MedicineController.java
+++ Hospital_Management_System/src/main/java/org/HMSApp/Hospital_Management_System/dologin/controller/MedicineController.java
@@ -1,14 +1,16 @@
 package org.HMSApp.Hospital_Management_System.dologin.controller;
 
 import java.util.List;
-
-import org.HMSApp.Hospital_Management_System.dologin.entity.Appointment;
+import javax.management.AttributeNotFoundException;
 import org.HMSApp.Hospital_Management_System.dologin.entity.Medicine;
 import org.HMSApp.Hospital_Management_System.dologin.repository.MedicineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +33,37 @@ public class MedicineController {
 	return medicineRepository.save(medicine);  
   }
   
+//  @PutMapping("{id}")
+//  public ResponseEntity<Medicine>updateMedicine(@PathVariable long id,@RequestBody Medicine  medicineDetails) throws AttributeNotFoundException
+//  {
+//	  Medicine medicine=medicineRepository.findById(id).orElseThrow(()->new AttributeNotFoundException("Medicine not found with id " + id));	
+//	  medicine.setDrugName(medicineDetails.getDrugName());
+//	  medicine.setStock(medicineDetails.getStock());		 
+//	  Medicine savedMedicine= medicineRepository.save(medicine);	 
+//		 return ResponseEntity.ok(savedMedicine);	
+//	 }
+  
+   @PutMapping("{id}")
+   public ResponseEntity<Medicine>updateMedicine(@PathVariable long id,@RequestBody Medicine medicineDetails)throws AttributeNotFoundException
+   {
+	  Medicine medicine =medicineRepository.findById(id).orElseThrow(()->new AttributeNotFoundException("Medicine not found with id"+id));
+	  medicine.setDrugName(medicineDetails.getDrugName());
+	  medicine.setStock(medicineDetails.getStock());
+	  
+	  Medicine saveMedicine =medicineRepository.save(medicine);
+	  
+	  return ResponseEntity.ok(saveMedicine);
+	   
+   }
+
+  
+  @GetMapping("/{id}")
+  public ResponseEntity<Medicine>getMedicineById(@PathVariable long id) throws AttributeNotFoundException 
+  {
+	  Medicine medicine=medicineRepository.findById(id).orElseThrow(()->new AttributeNotFoundException("Medicine not found with id"+id));
+ 	
+ 	return ResponseEntity.ok(medicine);
+  }
  
   
 }
